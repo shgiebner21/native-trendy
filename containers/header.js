da69724452c6797f84c6d264168669355f9b2645
Stephen Giebner <shgiebner21@gmail.com>
2017-05-23 09:15:23 -0400

    fixed StyleSheet.create({}) issue in header.js

--- containers/header.js
+++ containers/header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import NativeTachyons from 'react-native-style-tachyons'
 import { wrap } from 'react-native-style-tachyons'
 import { Constants } from 'expo'
 
 const Header = () => {
   return (
-    <View cls='h3' style={styles.container}>
+    <View cls='h3' style={[styles.container]}>
       <Text>I am a Header</Text>
     </View>
   )
